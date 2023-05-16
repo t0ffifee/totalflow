97a56083245dab88844371c368e3dff32c857ae3
Toufik Ammoura <toufikammoura@gmail.com>
2023-05-16 17:57:42 +0200

    basic stopwatch

---
+++ README.md
@@ -0,0 +1,11 @@
+# total flow
+
+## needed features
+ - start a stopwatch when you want to focus (and stop when done)
+ - save the amount of focused time
+    - total focused time of past 7 days can be shown in graph
+ - have a daily focus time goal
+ - sync focus data between extensions
+
+## possible additional features
+ - option to block sites
---
+++ totalflow-extension/scripts/stopwatch.js
@@ -0,0 +1,95 @@
+// GLOBAL VARIABLES
+var startTime;
+var timerIntervalID;
+
+// EVENT LISTENERS
+document.getElementById('mainButton').addEventListener('click', router);
+document.addEventListener('DOMContentLoaded', reloadExtension);
+
+function router() {
+  console.log("clicked on main button");
+  var running = localStorage.getItem('running');
+  if (running == 'true') {
+    console.log('going to homepage');
+    initHomePage();
+  } else {
+    console.log('going to workpage');
+    initWorkPage();
+  }
+}
+
+function reloadExtension() {
+  var running = localStorage.getItem('running');
+  if (running == 'true') {
+    initWorkPage();
+  } else {
+    initHomePage();
+  }
+}
+
+function initHomePage() {
+  // UI
+  // CHANGE HTML ELEMENTS
+  var subText = document.getElementById('subText');
+  subText.innerHTML = 'Start the timer and get into the flow';
+
+  var mainButton = document.getElementById('mainButton');
+  mainButton.innerHTML = "Start Working"
+
+  var stopwatch = document.getElementById('stopwatch');
+  stopwatch.innerHTML = '';
+
+  // LOGIC
+  // UPDATING VARIABLES
+  localStorage.setItem('running', false);
+  localStorage.setItem('startTime', null);
+
+  // STOPPING THE STOPWATCH UPDATES
+  clearInterval(timerIntervalID);
+}
+
+function initWorkPage() {
+  // UI
+  // CHANGE HTML ELEMENTS
+  var subText = document.getElementById('subText');
+  subText.innerHTML = 'Go get some shit done';
+
+  var mainButton = document.getElementById('mainButton');
+  mainButton.innerHTML = 'Stop Working';
+
+  // LOGIC
+  // UPDATING VARIABLES
+  localStorage.setItem('running', true);
+
+  startTime = localStorage.getItem('startTime');
+  if (startTime == 'null') {
+    startTime = Date.now();
+    localStorage.setItem('startTime', startTime);
+  }
+
+  timerIntervalID = setInterval(displayStopwatch, 50);
+}
+
+function displayStopwatch() {
+  var now = Date.now();
+  var elapsed = now - startTime;
+
+  // array of time multiples [hours, min, sec, decimal]
+  var d = [3600000, 60000, 1000, 10];
+  var time = [];
+  var i = 0;
+
+  while (i < d.length) {
+    var t = Math.floor(elapsed / d[i]);
+
+    // remove parsed time for next iteration
+    elapsed -= t * d[i];
+
+    // add '0' prefix to m,s,d when needed
+    t = (i > 0 && t < 10) ? '0' + t : t;
+    time.push(t);
+    i++;
+  }
+
+  document.getElementById('stopwatch').innerHTML = time[0] + ':' + time[1] + ':' + time[2];
+}
